6849396d74e324f0042173dbc7446ee013b10968
Tom Fogal <tfogal@alumni.unh.edu>
2008-07-27 02:56:26 -0400

    * Basic README * Made it clear that libtprof is covered by the LGPL.

--- src/recurse.c
+++ src/recurse.c
@@ -1,3 +1,19 @@
+/*
+ * This file is part of libtprof.
+ *
+ * libtprof is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libtprof is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libtprof.  If not, see <http://www.gnu.org/licenses/>.
+ */
 /** Simple program to test recursion with libtprof. */
 #include <stdio.h>
 #include <glib.h>
--- src/tprof.c
+++ src/tprof.c
@@ -1,5 +1,19 @@
-/** Tom Fogal, Thu Jul 24 17:33:58 EDT 2008
- * Simple profiler code. */
+/*
+ * This file is part of libtprof.
+ *
+ * libtprof is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * libtprof is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with libtprof.  If not, see <http://www.gnu.org/licenses/>.
+ */
 #ifndef _POSIX_C_SOURCE
 #define _POSIX_C_SOURCE 200112L
 #endif /* !_POSIX_C_SOURCE */
